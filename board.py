f16d8e8c0de1c4b767d61446411ded9a69135717
Aryan Prodduturi <32280917+SmashMineGame@users.noreply.github.com>
2022-08-05 22:38:58 -0700

    tweaks to checkWin

--- board.py
+++ board.py
@@ -50,26 +50,37 @@ class C4Board:
             return
         self.state[pos][top] = self.curr
         self.tops[pos] += 1
-        self.checkWin()
+        self.checkWin(pos)
         self.curr = 1 if self.curr == 2 else 2
 
-    def checkWin(self):
+    def checkWin(self, mx):
+        my = self.tops[mx] - 1
         cur = self.curr
+
         vcount = 0
-        hcounts = [0] * 6
+        for y in range(6):
+            p = self.state[mx][y]
+            if p == cur:
+                vcount += 1
+            else:
+                vcount = 0
 
-        # vertical connects
+            if vcount == 4:
+                self.winner = cur
+                self.gameOver = True
+                self.selected = -1
+                return
+
+        hcount = 0
         for x in range(7):
-            for y in range(6):
-                p = self.state[x][y]
-                if p == cur:
-                    vcount += 1
-                    hcounts[y] += 1
-                else:
-                    vcount = 0
-                    hcounts[y] = 0
+            p = self.state[x][my]
+            if p == cur:
+                hcount += 1
+            else:
+                hcount = 0
 
-                if vcount == 4 or hcounts[y] == 4:
-                    self.winner = cur
-                    self.gameOver = True
-                    self.selected = -1
+            if hcount == 4:
+                self.winner = cur
+                self.gameOver = True
+                self.selected = -1
+                return
